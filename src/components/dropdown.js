26129aa8ec7f7273e4cdf1e233edf41a29e3d236
saarakentta <127877216+saarakentta@users.noreply.github.com>
2023-11-20 16:50:59 +0200

    Dropdowns edited

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -5,16 +5,67 @@ import Dropdown from "react-bootstrap/Dropdown";
 function AppDropdown() {
     return (
       <>
+
+<Dropdown data-bs-theme="dark">
+          <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
+            Merkki
+          </Dropdown.Toggle>
+  
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-2">Audi</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">BMW</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Mercedes Benz</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+        
         <Dropdown data-bs-theme="dark">
           <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
             Käyttövoima
           </Dropdown.Toggle>
-  
           <Dropdown.Menu>
             <Dropdown.Item href="#/action-2">Bensiini</Dropdown.Item>
             <Dropdown.Item href="#/action-3">Diesel</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
+
+        <Dropdown data-bs-theme="dark">
+          <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
+            Korimalli
+          </Dropdown.Toggle>
+  
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-2">Sedan</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Farmari</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Pakettiauto</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+
+        <Dropdown data-bs-theme="dark">
+          <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
+            Väri
+          </Dropdown.Toggle>
+  
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-2">Musta</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Valkoinen</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Punainen</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Sininen</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Hopea</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+
+        <Dropdown data-bs-theme="dark">
+          <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
+            Vuosimalli
+          </Dropdown.Toggle>
+  
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-2">2010 - 2015</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">2016 - 2020</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">2020 -</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+        
   
         <Dropdown data-bs-theme="dark">
           <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
