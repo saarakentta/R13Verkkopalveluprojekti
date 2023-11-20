863f6078f02b0dcdb9815a747852eae90d8f07ff
saarakentta <127877216+saarakentta@users.noreply.github.com>
2023-11-20 17:26:53 +0200

    navbar edited

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,22 +2,25 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
-
 function AppNavbar() {
   return (
     <>
       <Navbar bg="light" data-bs-theme="light">
         <Container>
-          <Navbar.Brand href="#home">Etusivu</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Audi</Nav.Link>
-            <Nav.Link href="#features">BMW</Nav.Link>
-            <Nav.Link href="#pricing">Mercedes Benz</Nav.Link>
+            <Nav.Link href="#home">Etusivu</Nav.Link>
           </Nav>
         </Container>
         <Container className="audi-logo">LOGO</Container>
         <Container className="bmw-logo">LOGO</Container>
         <Container className="mb-logo">LOGO</Container>
+
+        <Container>
+          <Nav className="cart">
+            <Nav.Link href="#features">Ota yhteyttä</Nav.Link>
+            <Nav.Link href="#home">Ostoskori</Nav.Link>
+          </Nav>
+        </Container>
       </Navbar>
     </>
   );
