d82edc2354240a65b6ee1dff3337067257990f6f
Joonas <n2lijo00@students.oamk.fi>
2023-11-21 11:01:17 +0200

    cart

--- src/actioncomponents/shoppingcart.js
+++ src/actioncomponents/shoppingcart.js
@@ -1,19 +1,53 @@
-// shoppingcart.js
+// Import necessary dependencies
+import React, { useState } from 'react';
 
-import React from 'react';
+// Create a functional component for the shopping cart
+const ShoppingCart = () => {
+  // Set up state to manage the cart items
+  const [cartItems, setCartItems] = useState([]);
 
-const ShoppingCart = ({ cartItems, removeFromCart }) => {
+  // Function to add an item to the cart
+  const addItemToCart = (item) => {
+    setCartItems([...cartItems, item]);
+  };
+
+  // Function to remove an item from the cart
+  const removeItemFromCart = (index) => {
+    const updatedCart = [...cartItems];
+    updatedCart.splice(index, 1);
+    setCartItems(updatedCart);
+  };
+
+  // Function to calculate the total price of items in the cart
+  const calculateTotal = () => {
+    return cartItems.reduce((total, item) => total + item.price, 0).toFixed(2);
+  };
+
   return (
     <div>
-      <h2>Shopping Cart</h2>
+      <h1>Shopping Cart</h1>
+      
+      {/* Display the list of items in the cart */}
       <ul>
         {cartItems.map((item, index) => (
           <li key={index}>
-            {item.name} - {item.price}$
-            <button onClick={() => removeFromCart(index)}>Remove</button>
+            {item.name} - ${item.price}
+            <button onClick={() => removeItemFromCart(index)}>Remove</button>
           </li>
         ))}
       </ul>
+
+      {/* Display the total price of items in the cart */}
+      <p>Total: ${calculateTotal()}</p>
+
+      {/* Example products */}
+      <h2>Available Products</h2>
+      <button onClick={() => addItemToCart({ name: 'Product 1', price: 10.99 })}>
+        Add Product 1 to Cart
+      </button>
+      <button onClick={() => addItemToCart({ name: 'Product 2', price: 20.49 })}>
+        Add Product 2 to Cart
+      </button>
     </div>
   );
 };
