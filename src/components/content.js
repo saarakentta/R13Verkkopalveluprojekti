d80ff1d46a58742a5ae6e13194398136b4a9628e
saarakentta <127877216+saarakentta@users.noreply.github.com>
2023-11-24 10:20:14 +0200

    edited card to layout

--- src/components/content.js
+++ src/components/content.js
@@ -1,12 +1,22 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 function Content() {
     return (
-        <div>
-            <img className="content" src={"/images/oldMB.jpg"}></img>
-        </div>
-    )
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="./images/oldMB.jpg" />
+      <Card.Body>
+        <Card.Title>Product</Card.Title>
+        <Card.Text>
+          ABC-123, Mercedes Benz, 350X, Sedan, 2004, 30 000€, Diesel, Sininen 
+        </Card.Text>
+        <Button style={{ backgroundColor: '#176B87' }}>Lisää ostoskoriin</Button>
+      </Card.Body>
+    </Card>
+  );
 }
+     // <div><img className="content" src={"/images/oldMB.jpg"}></img>   </div>
 
 
 
