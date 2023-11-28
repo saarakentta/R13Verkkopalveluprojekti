84390f6f4168db7cd0f2e62ef39ea1aab644262c
viljamilaakso <n2lavi00@students.oamk.fi>
2023-11-28 14:59:47 +0200

    new shoppingcart test

--- src/actioncomponents/shoppingcart.js
+++ src/actioncomponents/shoppingcart.js
@@ -1,53 +1,18 @@
-// Import necessary dependencies
-import React, { useState } from 'react';
+// ShoppingCart.js
+import React from 'react';
 
-// Create a functional component for the shopping cart
-const ShoppingCart = () => {
-  // Set up state to manage the cart items
-  const [cartItems, setCartItems] = useState([]);
-
-  // Function to add an item to the cart
-  const addItemToCart = (item) => {
-    setCartItems([...cartItems, item]);
-  };
-
-  // Function to remove an item from the cart
-  const removeItemFromCart = (index) => {
-    const updatedCart = [...cartItems];
-    updatedCart.splice(index, 1);
-    setCartItems(updatedCart);
-  };
-
-  // Function to calculate the total price of items in the cart
-  const calculateTotal = () => {
-    return cartItems.reduce((total, item) => total + item.price, 0).toFixed(2);
-  };
-
+const ShoppingCart = ({ cart, updateCart, checkout }) => {
   return (
     <div>
-      <h1>Shopping Cart</h1>
-      
-      {/* Display the list of items in the cart */}
-      <ul>
-        {cartItems.map((item, index) => (
-          <li key={index}>
-            {item.name} - €{item.price}
-            <button onClick={() => removeItemFromCart(index)}>Remove</button>
-          </li>
-        ))}
-      </ul>
-
-      {/* Display the total price of items in the cart */}
-      <p>Total: €{calculateTotal()}</p>
-
-      {/* Example products */}
-      <h2>Available Products</h2>
-      <button onClick={() => addItemToCart({ name: 'bemari 520', price: 47000.00 })}>
-        Osta bemari
-      </button>
-      <button onClick={() => addItemToCart({ name: 'mersu e250', price: 49000.00 })}>
-        Osta mersu
-      </button>
+      <h2>Shopping Cart</h2>
+      {cart && Object.keys(cart).map((productId) => (
+        <div key={productId} style={{ border: '1px solid #ccc', padding: '10px', margin: '10px' }}>
+          <h3>{cart[productId].name}</h3>
+          <p>Quantity: {cart[productId].quantity}</p>
+          <button onClick={() => updateCart(productId, -1)}>Remove One</button>
+        </div>
+      ))}
+      <button onClick={checkout} style={{ marginTop: '10px' }}>Checkout</button>
     </div>
   );
 };
