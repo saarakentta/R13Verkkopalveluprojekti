f965de88c9cf3eadf65da629e1dc5fec740e3082
viljamilaakso <n2lavi00@students.oamk.fi>
2023-11-28 15:25:58 +0200

    remove button

--- src/actioncomponents/shoppingcart.js
+++ src/actioncomponents/shoppingcart.js
@@ -1,15 +1,15 @@
 // ShoppingCart.js
 import React from 'react';
 
-const ShoppingCart = ({ cart, updateCart, checkout }) => {
+const ShoppingCart = ({ cart, updateCart, removeItem, checkout }) => {
   return (
     <div>
       <h2>Shopping Cart</h2>
-      {cart && Object.keys(cart).map((productId) => (
-        <div key={productId} style={{ border: '1px solid #ccc', padding: '10px', margin: '10px' }}>
-          <h3>{cart[productId].name}</h3>
-          <p>Quantity: {cart[productId].quantity}</p>
-          <button onClick={() => updateCart(productId, -1)}>Remove One</button>
+      {cart && cart.map((item) => (
+        <div key={item.rekisteritunnus} style={{ border: '1px solid #ccc', padding: '10px', margin: '10px' }}>
+          <h3>{item.name}</h3>
+          <p>Quantity: {item.quantity}</p>
+          <button onClick={() => updateCart(item.rekisteritunnus, -1)}>Remove One</button>
         </div>
       ))}
       <button onClick={checkout} style={{ marginTop: '10px' }}>Checkout</button>
