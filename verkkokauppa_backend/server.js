42ea1e61ff593755ccce9debf102eeaa0769f0b6
Julia <n2poju01@students.oamk.fi>
2023-12-04 14:34:43 +0200

    Update server.js

--- verkkokauppa_backend/server.js
+++ verkkokauppa_backend/server.js
@@ -1,13 +1,12 @@
 require('dotenv').config()
-const mysql = require('mysql2/promise');
-const cors = require('cors');
-const express = require('express');
-const app = express();
+const mysql = require('mysql2/promise')
+const cors = require('cors')
+const express = require('express')
+const app = express()
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cors());
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors())
 
 const conf = {
     host: process.env.DB_HOST,
@@ -21,25 +20,98 @@ const conf = {
 const port = process.env.DB_PORT
 
 app.get('/products', async (req, res) => {
+    let connection;
+
+    const generateDynamicQuery = (filters) => {
+    const conditions = []
+    const values = []
+
+      for (const filter in filters) {
+          if (filters.hasOwnProperty(filter) && filters[filter] !== undefined && filters[filter] !== '') {
+            let condition
+            let filterValue
+
+            if (filter === 'hinta') {
+              const numericValues = filters[filter].split('-').map((val) => parseFloat(val.replace(/[^\d.]/g, '')))
+
+              if (filters[filter].includes('Yli')) {
+                condition = 'hinta > ?'
+                filterValue = numericValues[0]
+              } else if (filters[filter].includes('Alle')) {
+                condition = 'hinta < ?'
+                filterValue = numericValues[0]
+              } else {
+                condition = 'hinta BETWEEN ? AND ?'
+                filterValue = numericValues.length === 1 ? numericValues[0] : numericValues
+                const rangeValues = filters[filter].split('-').map(val => parseFloat(val.replace(/[^\d.]/g, '')))
+                filterValue = rangeValues.length === 1 ? rangeValues[0] : rangeValues
+
+              }
+            } else {
+              condition = `${filter} = ?`
+              filterValue = filters[filter]
+            }
+              conditions.push(condition)
+              values.push(filterValue)
+          }
+      }
+
+      if (conditions.length === 0) {
+          return { query: '', values: [] }
+      }
+
+      return {query: `WHERE ${conditions.join(' AND ')}`, values }
+  }
+
     try {
-        const connection = await mysql.createConnection(conf);
+      connection = await mysql.createConnection(conf)
+      const { merkki, korimalli, vari, kayttovoima, hinta } = req.query
+      const { query:  dynamicQuery, values } = generateDynamicQuery({ merkki, korimalli, vari, kayttovoima, hinta})
 
-        let merkki = req.query.merkki;
+      let result
 
-        let result;
+        if (dynamicQuery !== '') {
+        const filterQuery = `SELECT * FROM product ${dynamicQuery}`
 
-        if(merkki){
-            result = await connection.execute("SELECT * FROM product WHERE merkki=?", [merkki]);
-        }else{
-            result = await connection.execute("SELECT * FROM product");
+        result = await connection.execute(filterQuery, [].concat(...values))
+
+        } else {
+        result = await connection.execute('SELECT * FROM product');
         }
 
-        //First index in the result contains the rows in an array
-        res.json(result[0]);
+      const filteredProducts = result[0];
 
+      res.json(filteredProducts)
     } catch (err) {
-        res.status(500).json({ error: err.message });
+      res.status(500).json({ error: err.message })
+    } finally {
+        if (connection)
+          await connection.end()
     }
-});
+})
 
-app.listen(port)
+app.get('/filter-options/:column', async (req, res) => {
+  let connection
+  const createFilterQuery = (column) => `SELECT DISTINCT ${column} FROM product`
+
+  try {
+    connection = await mysql.createConnection(conf)
+    const { column } = req.params
+
+    const query = createFilterQuery(column)
+
+    const [result] = await connection.execute(query)
+
+    res.json(result.map((row) => row[column]))
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  } finally {
+    if (connection) {
+      await connection.end()
+    }
+  }
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
