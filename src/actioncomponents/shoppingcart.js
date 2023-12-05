458790efcac3c6a99fc42eb1d4a13983d0817bbb
Joonas <n2lijo00@students.oamk.fi>
2023-12-05 14:17:35 +0200

    consolelog

--- src/actioncomponents/shoppingcart.js
+++ src/actioncomponents/shoppingcart.js
@@ -5,13 +5,18 @@ const ShoppingCart = ({ cart, updateCart, removeItem, checkout }) => {
   return (
     <div>
       <h2>Shopping Cart</h2>
-      {cart && cart.map((item) => (
-        <div key={item.id} style={{ border: '1px solid #ccc', padding: '10px', margin: '10px' }}>
-          <h3>{item.id}</h3>
-          <p>Korissa: {item.malli}</p>
-          <button onClick={() => updateCart(item.id, -1)}>Remove One</button>
-        </div>
-      ))}
+      {cart && cart.map((item) => {
+        console.log("Current Item:", item);
+
+        return (
+          <div key={item.id} style={{ border: '1px solid #ccc', padding: '10px', margin: '10px' }}>
+            {/* Display 'malli' instead of 'id' */}
+            <h3>{item.malli}</h3>
+            <p>Korissa: {item.malli}</p>
+            <button onClick={() => updateCart(item.id, -1)}>Remove One</button>
+          </div>
+        );
+      })}
       <button onClick={checkout} style={{ marginTop: '10px' }}>Checkout</button>
     </div>
   );
