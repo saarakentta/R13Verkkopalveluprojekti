0d4fbb64c015772eee0d5ffd7507ad2163a7b87a
Joonas <n2lijo00@students.oamk.fi>
2023-12-05 13:41:37 +0200

    tyyli yhteistietoon

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,17 +1,35 @@
-import React from "react"
+import React from "react";
 
-function Contact() {
+const Contact = () => {
+  const contactContainerStyle = {
+    maxWidth: "400px",
+    margin: "auto",
+    textAlign: "center",
+    padding: "20px",
+    border: "1px solid #ccc",
+    borderRadius: "8px",
+    boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
+    backgroundColor: "#f8f8f8",
+  };
 
-    return(
-    <div>
-        <h2>Yhteystiedot</h2>
-        <h5>info@r13autokauppa.fi</h5>
-        <h5>Puh. 040123123123</h5>
-        <h5>Autokaupantie 1</h5>
-        <h5>90123, Oulu</h5>
-        <h2>Jätä palautteesi</h2>
+  const headingStyle = {
+    color: "#333",
+  };
+
+  const infoStyle = {
+    fontSize: "1.1em",
+    marginBottom: "10px",
+  };
+
+  return (
+    <div style={contactContainerStyle}>
+      <h2 style={headingStyle}>Yhteystiedot</h2>
+      <p style={infoStyle}>Sähköposti: info@r13autokauppa.fi</p>
+      <p style={infoStyle}>Puhelin: 040123123123</p>
+      <p style={infoStyle}>Osoite: Autokaupantie 1, 90123 Oulu</p>
+      <h2 style={headingStyle}>Jätä palautteesi</h2>
     </div>
-    )
-}
+  );
+};
 
 export default Contact;
