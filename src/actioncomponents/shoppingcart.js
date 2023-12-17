b2c1291faa6b4bfd41b586e9bc0f8d0395aedf51
Timbekki <c2kati01@students.oamk.fi>
2023-12-17 17:17:35 +0200

    shoppingcart hienosäätöä

--- src/actioncomponents/shoppingcart.js
+++ src/actioncomponents/shoppingcart.js
@@ -5,20 +5,15 @@ import axios from 'axios';
 import { jwtDecode } from 'jwt-decode';
 
 const ShoppingCart = ({ cart, removeFromCart, updateQuantity, emptyCart }) => {
-  // Calculate the total price of the shopping cart
-  const calculateTotalPrice = () => {
-    return cart.reduce((total, item) => total + item.total, 0);
-  };
+  const [notification, setNotification] = useState(null);
+  const showNotification = (message) => {
+    setNotification(message);
 
-const [notification, setNotification] = useState(null);
-const showNotification = (message) => {
-  setNotification(message);
-
-  //Piilota ilmoitus 3 sekunnin kuluttua
-  setTimeout(() => {
-    setNotification(null);
-  }, 3000)
-}
+    //Piilota ilmoitus 3 sekunnin kuluttua
+    setTimeout(() => {
+      setNotification(null);
+    }, 3000)
+  }
 
   function order() {
 
@@ -30,40 +25,41 @@ const showNotification = (message) => {
       return;
     }
 
-    try{
+    try {
       const decodedToken = jwtDecode(token);
       const customerId = decodedToken.customerId;
 
-    const orderData = {
-      customer_id: customerId,
-      products: cart.map(item => ({
-        product_id: item.id,
-        quantity: item.quantity
-      }))
-    }
-    // Lähetä tilaus tietokantaan
-    axios
-    .post("http://localhost:3001/order", orderData, {
-      headers: {
-        'Authorization': `Bearer ${token}`,
-        'Content-Type': 'application/json',
-      },
-    })
-    .then((resp) => {
-      console.log('Tilaus lähetetty onnistuneesti. ID:', resp.data.orderId);
-      emptyCart();
-      showNotification('Tilauksesi on vastaanotettu. Kiitos!');
-    })
-    .catch((error) => {
-      console.error('Virhe tilauksen lähetyksessä:', error.message);
-      // Käske käyttäjää yrittämään uudelleen tai näytä virheilmoitus
-    });
+      const orderData = {
+        customer_id: customerId,
+        products: cart.map(item => ({
+          product_id: item.id,
+          quantity: item.quantity
+        }))
+      }
+      // Lähetä tilaus tietokantaan
+      axios
+        .post("http://localhost:3001/order", orderData, {
+          headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+        })
+        .then((resp) => {
+          console.log('Tilaus lähetetty onnistuneesti. ID:', resp.data.orderId);
+          emptyCart();
+          showNotification('Tilauksesi on vastaanotettu. Kiitos!');
+        })
+        .catch((error) => {
+          console.error('Virhe tilauksen lähetyksessä:', error.message);
+          showNotification('Virhe tilauksen lähetyksessä, yritä myöhemmin uudelleen.');
+          // Käske käyttäjää yrittämään uudelleen tai näytä virheilmoitus
+        });
 
-} catch (error) {
-  console.error('Virhe purkaessa JWT-tokenia:', error.message);
-  // Käske käyttäjää yrittämään uudelleen tai näytä virheilmoitus
-}
-}
+    } catch (error) {
+      console.error('Virhe purkaessa JWT-tokenia:', error.message);
+      // Käske käyttäjää yrittämään uudelleen tai näytä virheilmoitus
+    }
+  }
 
   return (
     <div>
@@ -74,7 +70,7 @@ const showNotification = (message) => {
           <p>Hinta: {item.hinta}€</p>
           <p>Määrä: {item.quantity}</p>
           <p>Yhteensä: {item.total}€</p>
-          <button onClick={() => updateQuantity(item.id, item.quantity - 1)}disabled={item.quantity <= 1}>
+          <button onClick={() => updateQuantity(item.id, item.quantity - 1)} disabled={item.quantity <= 1}>
             Vähennä
           </button>
           <button className='lisaa' onClick={() => updateQuantity(item.id, item.quantity + 1)}>
@@ -84,9 +80,9 @@ const showNotification = (message) => {
         </div>
       ))}
       <div>
-      <button className='empty' onClick={emptyCart}>Tyhjennä ostoskori</button>
-      <button className='send' onClick={order}>Lähetä tilauksesi</button>
-      {notification && <div style={{fontSize: '20px', color: 'green', marginLeft: '10px', marginBottom: '5px'}}>{notification}</div>}
+        <button className='empty' onClick={emptyCart}>Tyhjennä ostoskori</button>
+        <button className='send' onClick={order}>Lähetä tilauksesi</button>
+        {notification && <div style={{ fontSize: '20px', color: 'green', marginLeft: '10px', marginBottom: '5px' }}>{notification}</div>}
       </div>
     </div>
   );
